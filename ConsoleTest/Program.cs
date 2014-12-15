9d894846c34bc0c4bf8c99a49ece92b2c3e1d30d
Rajesh Vaghela <rajesh.vaghela@igate.com>
2014-12-15 16:10:50 +0530

    Update Program.cs

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -10,8 +10,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            var abc = "Hello world 1";
-            abc = abc.Trim();
+            var abc = "Hello world 12";
             abc = abc.Trim();
         }
     }
